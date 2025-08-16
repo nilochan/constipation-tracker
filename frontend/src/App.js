c3926c9dc461840aac5337c94e294d0b8803f557
nilochan <chanchinthai@hotmail.com>
2025-08-16 22:54:41 +0800

    Fix import reference to correct ConstipationReliefTracker component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ConstipationReliefTracker from './ConstipationReliefTracker-Professional';
+import ConstipationReliefTracker from './ConstipationReliefTracker';
 import './App.css';
 
 function App() {
